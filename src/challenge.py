ecce986667433838c31cc36140de8ee23720fc43
danibaxx <danibaxx@hotmail.com>
2020-08-05 18:32:03 -0600

    challenge with TL

---
+++ src/challenge.py
@@ -0,0 +1,13 @@
+# Print out each element of the following array on a separate line:
+
+items = ["Joe", "2", "Ted", "4.98", "14", "Sam", "void *", "42", "float", "pointers", "5006"]
+
+# You may use whatever programming language you’d like.
+# Verbalize your thought process as much as possible before writing any code.
+# Run through the UPER problem solving framework while going through your thought process.
+
+
+# for loop into original list
+# print each item from list
+for x in items:
+    print(x)
